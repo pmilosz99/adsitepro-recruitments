170b3a83a76666f4a4c617faa967ad2cf37e5aec
pmilosz99 <piter31@poczta.onet.pl>
2024-09-11 23:19:19 +0200

    remove unnecessary code

--- src/components/GallerySection/gallery-section.tsx
+++ src/components/GallerySection/gallery-section.tsx
@@ -2,7 +2,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
-import './style.css';
 
 import { Pagination } from 'swiper/modules';
 import { useState } from 'react';
